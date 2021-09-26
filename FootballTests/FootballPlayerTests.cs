874178423fb3f165fd7669b60e763d43cbc45b48
Kermac11 <73934167+Kermac11@users.noreply.github.com>
2021-09-26 23:23:03 +0200

    Initial commit

---
+++ FootballTests/FootballPlayerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballplayerTests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        [TestMethod()]
+        public void FootballPlayerConstructorTest()
+        {
+            FootballPlayer testplayer = new FootballPlayer(1, "John", 50, 1);
+            Assert.IsNotNull(testplayer);
+
+            Assert.ThrowsException<ArgumentException>(() => testplayer = new FootballPlayer(1, "Per", 50, 1));
+
+            Assert.ThrowsException<ArgumentException>(() => testplayer = new FootballPlayer(1, "John", 0, 1));
+
+            Assert.ThrowsException<ArgumentException>(() => testplayer = new FootballPlayer(1, "John", 50, 2001));
+
+        }
+
+        [TestMethod()]
+        public void PropertyChangeTest()
+        {
+            FootballPlayer testplayer = new FootballPlayer(1, "John", 50, 1);
+            Assert.IsNotNull(testplayer);
+
+            Assert.ThrowsException<ArgumentException>(() => testplayer.Name = "Ib");
+
+            Assert.ThrowsException<ArgumentException>(() => testplayer.Price = -20);
+
+            Assert.ThrowsException<ArgumentException>(() => testplayer.ShirtNumber = 200);
+
+            Assert.ThrowsException<ArgumentException>(() => testplayer.ShirtNumber = 0);
+
+        }
+    }
+}
---
+++ ModelLib/FootballPlayer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ModelLib
+{
+    public class FootballPlayer
+    {
+        private int _id;
+        private string _name;
+        private double _price;
+        private int _shirtNumber;
+
+        public FootballPlayer(int id, string name, double price, int shirtNumber)
+        {
+            _id = id;
+            if (!(name.Length >= 4))
+            {
+                throw new ArgumentException("Name must be minimum 4 letters or more");
+            }
+            _name = name;
+            if (!(price > 0))
+            {
+                throw new ArgumentException("Price must be above 0");
+            }
+            _price = price;
+            if (!(shirtNumber >= 1 && shirtNumber <= 100))
+            {
+                throw new ArgumentException("Number must be beetween 1 and 100");
+            }
+            _shirtNumber = shirtNumber;
+        }
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (!(value.Length >= 4))
+                {
+                    throw new ArgumentException("Name must be minimum 4 letters or more");
+                }
+                _name = value;
+            }
+        }
+
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (!(value > 0))
+                {
+                    throw new ArgumentException("Price must be above 0");
+                }
+                _price = value;
+            }
+        }
+
+        public int ShirtNumber
+        {
+            get => _shirtNumber;
+            set
+            {
+                if (!(value >= 1 && value <= 100))
+                {
+                    throw new ArgumentException("Number must be beetween 1 and 100");
+                }
+                _shirtNumber = value;
+            }
+        }
+    }
+}
